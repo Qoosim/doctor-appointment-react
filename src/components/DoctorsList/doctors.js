aac172124a7c00bb8b97065c0ce8fec5d7b996bf
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-11-27 18:32:19 +0100

    Add carousel to the doctors page

---
+++ src/components/DoctorsList/doctors.js
@@ -0,0 +1,34 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const doctors = [
+  {
+    id: uuidv4(),
+    name: 'Dr Kassy',
+    photo: 'https://res.cloudinary.com/qoosim/image/upload/v1669557091/doctor-appointment-react-app/doc-Kassy_dkb3du.jpg',
+    desc: 'Hello there, nice to meet you',
+  },
+  {
+    id: uuidv4(),
+    name: 'Dr John',
+    photo: 'https://res.cloudinary.com/qoosim/image/upload/v1669557122/doctor-appointment-react-app/doc-John_ks7k27.jpg',
+    desc: 'Hello there, nice to meet you',
+  },
+  {
+    id: uuidv4(),
+    name: 'Dr Kate',
+    photo: 'https://res.cloudinary.com/qoosim/image/upload/v1669557147/doctor-appointment-react-app/doc-Kate_wvdnra.jpg',
+    desc: 'Hello there, nice to meet you',
+  },
+  {
+    id: uuidv4(),
+    name: 'Dr Joe',
+    photo: 'https://res.cloudinary.com/qoosim/image/upload/v1669557163/doctor-appointment-react-app/doc-Joe_mg3ws0.jpg',
+    desc: 'Hello there, nice to meet you',
+  },
+  {
+    id: uuidv4(),
+    name: 'Dr Mercy',
+    photo: 'https://res.cloudinary.com/qoosim/image/upload/v1669557176/doctor-appointment-react-app/doc-Mercy_cka0ty.jpg',
+    desc: 'Hello there, nice to meet you',
+  }
+]
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { navLinks } from "./navLinks";
 import { socialLinks } from "./socialLinks";
 import styles from "./sidebar.module.css";
